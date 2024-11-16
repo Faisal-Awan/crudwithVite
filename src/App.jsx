55ce22810e57bb03be0c790257ef91e737223e43
Faisal Awan <faisal.awan3303@gmail.com>
2024-11-16 10:46:23 +0500

    without react Query

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import AppRoutes from './Routes';
-
-const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
+
+
+
+
+// import React from 'react';
+// import { BrowserRouter } from 'react-router-dom';
+// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+// import AppRoutes from './Routes';
+
+// const queryClient = new QueryClient();
+
+// export default function App() {
+//   return (
+//     <QueryClientProvider client={queryClient}>
+//       <BrowserRouter>
+//         <AppRoutes />
+//       </BrowserRouter>
+//     </QueryClientProvider>
+//   );
+// }
